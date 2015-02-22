0d4f66bd82c8f4e30ac8cb1a4c8edad70aa5d98e
Max Sveshnikov <msveshnikov@gmail.com>
2015-02-22 11:30:10 +0600

    import works

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -7,34 +7,40 @@ class Rate < ActiveRecord::Base
 
     usd = {
         time: a.series.x.nodes.first.split(';').map { |v| v.to_i },
-        val: a.series.y.nodes.first.split(';').map { |v| v.to_f.round }
+        val: a.series.y.nodes.first.split(';').map { |v| v.to_f }
     }
 
     oil = {
         time: b.series.x.nodes.first.split(';').map { |v| v.to_i },
-        val: b.series.y.nodes.first.split(';').map { |v| v.to_f.round }
+        val: b.series.y.nodes.first.split(';').map { |v| v.to_f }
     }
-
 
     eur = {
         time: c.series.x.nodes.first.split(';').map { |v| v.to_i },
-        val: c.series.y.nodes.first.split(';').map { |v| v.to_f.round }
+        val: c.series.y.nodes.first.split(';').map { |v| v.to_f }
     }
 
-    oil_rub = { time: [], val: [] }
-
     j = 0 # exchange rate for current time
-    usd_exc_rate = usd[:val][0]
-    oil[:time].each_with_index do |time, i|
-      while ((time > usd[:time][j+1]) rescue false)
+    k = 0
+    oil_rate = oil[:val][0]
+    eur_rate = eur[:val][0]
+    usd[:time].each_with_index do |time, i|
+      while ((time > oil[:time][j+1]) rescue false)
         j+= 1
-        usd_exc_rate = usd[:val][j]
+        oil_rate = oil[:val][j]
       end
+      while ((time > eur[:time][k+1]) rescue false)
+        k+= 1
+        eur_rate = eur[:val][k]
+      end
+      usd_rate=usd[:val][i]
 
-      oil_rub[:time] << time
-      oil_rub[:val] << usd_exc_rate * oil[:val][i] rescue 0
+      @rate = Rate.new
+      @rate.date=Time.at(time).to_datetime
+      @rate.dollar=usd_rate
+      @rate.oil=oil_rate
+      @rate.euro=eur_rate
+      @rate.save!
     end
-
-    oil_rub
   end
 end
