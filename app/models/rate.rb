73d488e5d472aa1d12b3c4a4b9f86c886598655a
Max Sveshnikov <msveshnikov@gmail.com>
2015-02-22 10:40:53 +0600

    sec

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,2 +1,40 @@
 class Rate < ActiveRecord::Base
+
+  def self.import
+    a = Ox.parse Net::HTTP.get('news.yandex.ru', '/quotes/graph_1.xml')
+    b = Ox.parse Net::HTTP.get('news.yandex.ru', '/quotes/graph_1006.xml')
+    c = Ox.parse Net::HTTP.get('news.yandex.ru', '/quotes/graph_23.xml')
+
+    usd = {
+        time: a.series.x.nodes.first.split(';').map { |v| v.to_i },
+        val: a.series.y.nodes.first.split(';').map { |v| v.to_f.round }
+    }
+
+    oil = {
+        time: b.series.x.nodes.first.split(';').map { |v| v.to_i },
+        val: b.series.y.nodes.first.split(';').map { |v| v.to_f.round }
+    }
+
+
+    eur = {
+        time: c.series.x.nodes.first.split(';').map { |v| v.to_i },
+        val: c.series.y.nodes.first.split(';').map { |v| v.to_f.round }
+    }
+
+    oil_rub = { time: [], val: [] }
+
+    j = 0 # exchange rate for current time
+    usd_exc_rate = usd[:val][0]
+    oil[:time].each_with_index do |time, i|
+      while ((time > usd[:time][j+1]) rescue false)
+        j+= 1
+        usd_exc_rate = usd[:val][j]
+      end
+
+      oil_rub[:time] << time
+      oil_rub[:val] << usd_exc_rate * oil[:val][i] rescue 0
+    end
+
+    oil_rub
+  end
 end
