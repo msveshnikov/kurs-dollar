3c3ffd95603c9ff3fca57a8b012514bdf478d309
Max Sveshnikov <msveshnikov@thumbtack.net>
2015-02-22 17:32:11 +0600

    fix

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
-gem 'ox'
+#gem 'ox'
 gem 'lazy_high_charts'
 
 # Use sqlite3 as the database for Active Record
@@ -30,14 +30,4 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.1.2'
 
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano', group: :development
-
-# Use debugger
-# gem 'debugger', group: [:development, :test]
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+class HomeController < ApplicationController
+  def index
+    @chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: "Динамика курсов")
+
+      params[:month]=1 if !params[:month]
+      @rates=Rate.where(date: params[:month].to_i.month.ago..Date.today)
+      @dollar=@rates.last.dollar
+      @euro=@rates.last.euro
+      @oil=@rates.last.oil
+      f.xAxis(categories: @rates.map { |v| v.date.to_s[0..9] })
+      f.series(name: "Курс доллара", yAxis: 0, data: @rates.map { |v| v.dollar })
+      f.series(name: "Курс евро", yAxis: 0, data: @rates.map { |v| v.euro })
+      f.series(name: "Цена на нефть", yAxis: 0, data: @rates.map { |v| v.oil })
+
+      f.yAxis [{ title: { text: "Курс доллара/евро", margin: 30 } }]
+      #{ title: { text: "Цена на нефть $/барр" }, opposite: true } ]
+
+      f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical',)
+      f.chart({ defaultSeriesType: "line" })
+    end
+  end
+end
